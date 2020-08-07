47e1c18469d11d8ef406c174c445aa67120bb152
Radek Podrazky <rpodrazky@securityscorecard.io>
2020-08-07 12:49:12 +0200

    Button component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -9,6 +9,7 @@ export default {
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
+    label: { defaultValue: 'Button' },
   },
 } as Meta;
 
@@ -16,23 +17,8 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  size: 'large',
 };
 
 export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
